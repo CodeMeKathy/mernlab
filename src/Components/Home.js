518b94c8795c455a91b67c3fcf1a08227cd7fc38
Kathy * <kathy.lambert@lotusdesignsagency.com>
2018-07-01 17:09:31 -0400

    Added new btn on home page.

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 import '../css/Home.css' 
 
 class Home extends Component {
     render() {
-        return (<div className="page-container Home">
-                <h1>Welcome</h1>
-                {/* <img src="https://github.com/Kathy145/familyCookbookManager/blob/master/public/img/welcome.png?raw=true" alt="" /> */}
-            </div>)
+        return (
+            <div className="page-container Home">
+                <div className='row'>
+                    <h1>Welcome</h1>
+                    {/* <img src="https://github.com/Kathy145/familyCookbookManager/blob/master/public/img/welcome.png?raw=true" alt="" /> */}
+                    <div className='home-btn'>
+                        <button className="btn">
+                        <Link to="/recipes">Recipes</Link>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        )
     }
 }
 
 export default Home
+  
